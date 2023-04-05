4c80865415260be82b643fe9e9946d54966f0c99
ndrvndr <andreavindra37@gmail.com>
2023-04-05 21:42:39 +0700

    setup quote page

---
+++ app/Quote/page.js
@@ -0,0 +1,9 @@
+export default function Quote() {
+  return (
+    <main className="text-white flex flex-col items-center">
+      <section>
+        <span className="font-bold text-2xl">QUOTE PAGE</span>
+      </section>
+    </main>
+  );
+}
