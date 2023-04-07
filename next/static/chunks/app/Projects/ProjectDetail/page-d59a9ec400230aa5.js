d8c2d02a490baff36a711c52d69207488ccff45e
ndrvndr <andreavindra37@gmail.com>
2023-04-07 15:32:15 +0700

    delete next folder

--- next/static/chunks/app/Projects/ProjectDetail/page-d59a9ec400230aa5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[166,990],{2136:function(n,e,u){Promise.resolve().then(u.t.bind(u,3920,23))}},function(n){n.O(0,[920,455,744],function(){return n(n.s=2136)}),_N_E=n.O()}]);
